533c9bf86539bc649df34c6898300f1de8df4368
kazuki-s <30779150+kazuki-s@users.noreply.github.com>
2025-08-06 10:26:21 +0900

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -9,7 +9,7 @@ export default async function handler(req) {
     const lineRes = await fetch('https://notify-api.line.me/api/notify', {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer あなたのLINEアクセストークン',
+        'Authorization': 'Bearer jzcN59ozbLmEoRNvZLDqqKR5F5knZfYJshH1WIWzS0/J1Qq3KFNrPAOj38fQSrbBWYZexpcee7ay1FKdFCQR/2XYT0WU/M6DzfpBpig6QQqW/wDya8A/HUutZ6ostNExr74OE+5xGyyEwezl3xH5LAdB04t89/1O/w1cDnyilFU=',
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: new URLSearchParams({ message }),
